41e0958e9546d52dbb46a2d5f596222eba8ad975
tkooooo123 <tkooooo123@gmail.com>
2022-02-01 19:01:53 +0800

    add static data to use bootstrap

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index')
