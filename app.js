d05add88a6040a761a5bd75a257581d027aa4f8d
tkooooo123 <tkooooo123@gmail.com>
2022-02-02 11:21:04 +0800

    add show page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const URL = require('./models/url')
 
 const app = express()
 
@@ -16,9 +18,24 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.get('/url', (req,res) => {
+    URL.find()
+    .lean()
+    .then(urls => res.render('show', {urls}))
+    .catch(error => console.log(error))
+})
+
+app.post('/urls', (req,res) => {
+    const name = req.body.name
+    return URL.create({name})
+    .then(() => res.redirect('/url'))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
