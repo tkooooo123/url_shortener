46c97810554154020d1c1bff907873a6da508ecd
tkooooo123 <tkooooo123@gmail.com>
2022-02-03 18:16:37 +0800

    修正 資料庫檢查不會重複新增 與 show頁面顯示短網址

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -1,25 +1,32 @@
 const express = require('express')
+const { redirect, append } = require('express/lib/response')
 const url = require('../../models/url')
 const router = express.Router ()
 const URL = require('../../models/url')
 const shortUrl = require('../../shorten_url')
 
 
-router.get('/url', (req,res) => {
-    URL.find()
-    .lean()
-    .then(urls => res.render('show', {urls}))
-    .catch(error => console.log(error))
-})
+
 
 router.post('/urls', (req,res) => {
     const url = req.body.url
     const code = shortUrl()
-    URL.findOne({url, shortUrl: code})
-    .then(data => data ? data : URL.create({url, shortUrl: code}))
-    .then(data => console.log(data))
     
-    .then(() => res.render('show', {url, shortUrl: code}))
+    URL.findOne({url}) 
+    .then(data => data ? data : URL.create({url, shortUrl: code}))
+    .then(data => {
+        console.log(data)
+        const short_url = `http://localhost:3000/${data.shortUrl}`
+        console.log(short_url)
+        res.render('show', {url, short_url})
+    })   
     .catch(error => console.log(error))
 })
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    
+})
+
+
 module.exports = router
