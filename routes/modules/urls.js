14355200b1cd5caf781a867fcde33fdbebd4e5db
tkooooo123 <tkooooo123@gmail.com>
2022-02-03 23:27:53 +0800

    修正路由

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -14,18 +14,20 @@ router.post('/urls', (req,res) => {
     
     URL.findOne({url}) 
     .then(data => data ? data : URL.create({url, shortUrl: code}))
-    .then(data => {
-        console.log(data)
-        const short_url = `http://localhost:3000/${data.shortUrl}`
-        console.log(short_url)
-        res.render('show', {url, short_url})
-    })   
+    .then(data => res.redirect(`/url/${data._id}`))   
     .catch(error => console.log(error))
 })
 
-router.get('/:id', (req, res) => {
+router.get('/url/:id', (req, res) => {
     const id = req.params.id
-    
+    URL.findById(id)
+    .lean()
+    .then(data => {
+        console.log(data)
+        const short_url = `http://localhost:3000/${data.shortUrl}`
+        res.render('show', {url, short_url})
+    })
+    .catch(error => console.log(error))
 })
 
 
